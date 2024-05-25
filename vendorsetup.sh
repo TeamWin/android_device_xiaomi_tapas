9a9782e6a9108334bd47af29b5bb8fc077ce4765
AspectPower <95100392+AspectPower@users.noreply.github.com>
2024-05-25 22:58:34 +0300

    vendorsetup.sh: TWRP Official Dependency Testing

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,8 @@ export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 export CCACHE_MAXSIZE="40G"
 export CCACHE_DIR="~/.ccache" 
+# TWRP OFFICIAL DEPENDENCY TEST
+sudo git clone --depth 1 --branch main --single-branch https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8 /opt/toolchains/arm-eabi-4.8/
 
 # Warn if CCACHE_DIR is an invalid directory
 if [ ! -d ${CCACHE_DIR} ];
